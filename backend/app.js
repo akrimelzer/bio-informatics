14a980ec8d0904d7c15a80fdcebac0403c7b1739
oddaspa <odd.gunnar.aspaas@gmail.com>
2020-02-12 16:58:09 +0100

    :tada: initialized backend in Koa with Hello World template

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const logger = require("koa-logger");
+const app = new Koa();
+
+// log all events to the terminal
+app.use(logger());
+
+// error handling
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit("error", err, ctx);
+  }
+});
+
+// instantiate our new Router
+const router = new Router();
+// require our external routes and pass in the router
+require("./routes/basic")({ router });
+
+// tells the router to use all the routes that are on the object
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// tells the server to listen to events on the 3000 port
+const server = app.listen(3000);
+module.exports = server;
