7e6aeb40cb0105fe840c06eb155148174f918a47
oddaspa <odd.gunnar.aspaas@gmail.com>
2020-02-12 18:24:24 +0100

    :sparkles: add dropdown menu

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -14,7 +14,27 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify
         </h1>
-        <v-btn @click="callBackend">Get collections</v-btn>
+        <v-menu
+          v-model="value"
+          :close-on-click="true"
+          :close-on-content-click="true"
+          :offset-y="true"
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn color="primary" dark v-on="on">
+              Dropdown
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(collection, index) in collections"
+              :key="index"
+              @click="doSomething"
+            >
+              <v-list-item-title>{{ collection.name }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-col>
     </v-row>
   </v-container>
@@ -24,18 +44,25 @@
 import axios from "axios";
 export default {
   name: "HelloWorld",
-  data: () => ({}),
+  data: () => ({
+    collections: "",
+    value: false
+  }),
   methods: {
-    callBackend: function() {
-      axios
-        .get("http://localhost:3000/collections/")
-        .then(function(response) {
-          console.log(response.data.results);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
+    doSomething: function() {
+      console.log("did something.");
     }
+  },
+  created: function() {
+    axios
+      .get("http://localhost:3000/collections/")
+      .then(response => {
+        this.collections = response.data.results;
+        console.log(this.collection);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
 </script>
