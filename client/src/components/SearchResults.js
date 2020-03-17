fc1f11ac8d684766353554fa1d16fde37960fe37
Andreas Melzer <akrimelzer@gmail.com>
2020-03-17 02:17:43 +0100

    :lipstick: Add grid for proteins

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { Card } from '@blueprintjs/core';
+import { Button } from '@blueprintjs/core';
 import { Colors } from '@blueprintjs/core';
 import styles from './SearchResults.module.css';
 
 function SearchResults(props) {
   const Result = (protein) => {
     return (
-      <Card
+      <Button
         onClick={() => props.openModal(protein.protein.name)}
         key={protein.matrix_id}
-        elevation='0'
-        interactive='true'
+        intent='primary'
+        large='true'
         style={{
-          backgroundColor: Colors.BLUE3,
           color: 'white',
-          width: '300px',
           height: '30px',
           lineHeight: '5px',
           marginBottom: '5px'
         }}>
         {protein.protein.name}
-      </Card>
+      </Button>
     );
   };
   //  New array based on the search-text from input
+
   let filteredMatrixes = props.matrixes.filter((protein) => {
     return protein.name.indexOf(props.search) !== -1;
   });
+
   return (
     <div className={styles.container}>
       {filteredMatrixes.map((result) => {
