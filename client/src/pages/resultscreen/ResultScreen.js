67020204715751c02840683d13628a42671d671d
karolbon <karolbon@stud.ntnu.no>
2020-03-19 19:31:34 +0100

    :sparkles: Navigate to result screen

--- client/src/pages/resultscreen/ResultScreen.js
+++ client/src/pages/resultscreen/ResultScreen.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import styles from "./ResultScreen.module.css";
+import { useLocation, Link } from "react-router-dom";
+import { Button } from "@blueprintjs/core";
 
 function ResultScreen() {
-  return <h1>Hello from result screen!</h1>;
+  const data = useLocation();
+
+  console.log(data);
+
+  const homeButton = (
+    <div className={styles.homeButton}>
+      <Link
+        to={{
+          pathname: "/"
+        }}
+      >
+        <Button intent="primary" icon="home" minimal="true" large="true">
+          Home
+        </Button>
+      </Link>
+    </div>
+  );
+
+  return (
+    <div>
+      {homeButton}
+      <h1>Result</h1>
+    </div>
+  );
 }
 
 export default ResultScreen;
