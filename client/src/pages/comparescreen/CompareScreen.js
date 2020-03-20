b06d2e1f5776da8fd941ffe51fe2dfdbc799da0e
Andreas Melzer <akrimelzer@gmail.com>
2020-03-20 16:33:46 +0100

    .

--- client/src/pages/comparescreen/CompareScreen.js
+++ client/src/pages/comparescreen/CompareScreen.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { Radio, RadioGroup, TextArea, Button } from "@blueprintjs/core";
-import { useLocation, Link } from "react-router-dom";
-import styles from "./CompareScreen.module.css";
-import { streptococcus_r6, chromosome_1 } from "../../var.js";
+import React, { useState, useEffect } from 'react';
+import { Radio, RadioGroup, TextArea, Button } from '@blueprintjs/core';
+import { useLocation, Link } from 'react-router-dom';
+import styles from './CompareScreen.module.css';
+import { streptococcus_r6, chromosome_1 } from '../../var.js';
 
 function CompareScreen() {
-  const [protein, setProtein] = useState("");
-  const [text, setText] = useState("");
-  const [selected, setSelected] = useState("custom");
+  const [protein, setProtein] = useState('');
+  const [text, setText] = useState('');
+  const [selected, setSelected] = useState('custom');
   const data = useLocation();
 
   useEffect(() => {
@@ -17,16 +17,16 @@ function CompareScreen() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const changeSelectedValue = e => {
-    if (e.currentTarget.value === "human") {
-      setSelected("human");
-      setText(chromosome_1.replace(/(\r\n|\n|\r)/gm, ""));
-    } else if (e.currentTarget.value === "strepto") {
-      setSelected("strepto");
-      setText(streptococcus_r6.replace(/(\r\n|\n|\r)/gm, ""));
+  const changeSelectedValue = (e) => {
+    if (e.currentTarget.value === 'human') {
+      setSelected('human');
+      setText(chromosome_1.replace(/(\r\n|\n|\r)/gm, ''));
+    } else if (e.currentTarget.value === 'strepto') {
+      setSelected('strepto');
+      setText(streptococcus_r6.replace(/(\r\n|\n|\r)/gm, ''));
     } else {
-      setSelected("custom");
-      setText("");
+      setSelected('custom');
+      setText('');
     }
   };
 
@@ -38,62 +38,57 @@ function CompareScreen() {
       <h3>Selected protein:</h3>
       <div className={styles.protein}>
         <h4>{protein.name}</h4>
-        <Link to="/">
+        <Link to='/'>
           <Button
-            icon="exchange"
-            intent="primary"
-            minimal="false"
-            style={{ marginTop: "13px", marginLeft: "10px" }}
-          >
+            icon='exchange'
+            intent='primary'
+            minimal='false'
+            style={{ marginTop: '13px', marginLeft: '10px' }}>
             Change
           </Button>
         </Link>
       </div>
       <div className={styles.genomeWrapper}>
-        <p style={{ textAlign: "start" }}>
+        <p style={{ textAlign: 'start' }}>
           Select one one the predefined genomes or chose custom to use a custom
           genome:
         </p>
         <div className={styles.radioGroup}>
           <RadioGroup
-            inline="true"
-            onChange={e => changeSelectedValue(e)}
-            selectedValue={selected}
-          >
+            inline='true'
+            onChange={(e) => changeSelectedValue(e)}
+            selectedValue={selected}>
             <Radio
-              label="Human (Chromosome 1)"
-              value="human"
-              large="true"
-            ></Radio>
-            <Radio label="Streptococcus" value="strepto" large="true"></Radio>
-            <Radio label="Custom" value="custom" large="true"></Radio>
+              label='Human (Chromosome 1)'
+              value='human'
+              large='true'></Radio>
+            <Radio label='Streptococcus' value='strepto' large='true'></Radio>
+            <Radio label='Custom' value='custom' large='true'></Radio>
           </RadioGroup>
         </div>
         <div className={styles.textInput}>
-          <p style={{ textAlign: "start" }}>Insert the sequence here:</p>
+          <p style={{ textAlign: 'start' }}>Insert the sequence here:</p>
           <TextArea
-            style={{ height: "250px" }}
+            style={{ height: '250px' }}
             growVertically={false}
             large={true}
             fill={true}
             small={true}
             value={text}
-            onChange={e => setText(e.target.value)}
+            onChange={(e) => setText(e.target.value)}
           />
         </div>
         <div className={styles.buttons}>
           <div className={styles.backButton}>
             <Link
               to={{
-                pathname: "/"
-              }}
-            >
+                pathname: '/'
+              }}>
               <Button
-                intent="primary"
-                icon="arrow-left"
-                minimal="true"
-                large="true"
-              >
+                intent='primary'
+                icon='arrow-left'
+                minimal='true'
+                large='true'>
                 Go back
               </Button>
             </Link>
@@ -101,17 +96,15 @@ function CompareScreen() {
           <div className={styles.continueButton}>
             <Link
               to={{
-                pathname: "/results",
+                pathname: '/results',
                 protein: { protein },
-                genome: selected
-              }}
-            >
+                genome: { text }
+              }}>
               <Button
-                intent="primary"
-                icon="arrow-right"
-                minimal="true"
-                large="true"
-              >
+                intent='primary'
+                icon='arrow-right'
+                minimal='true'
+                large='true'>
                 Continue
               </Button>
             </Link>
--- client/src/pages/resultscreen/ResultScreen.js
+++ client/src/pages/resultscreen/ResultScreen.js
@@ -1,23 +1,51 @@
-import React from "react";
-import styles from "./ResultScreen.module.css";
-import { useLocation, Link } from "react-router-dom";
-import { Button } from "@blueprintjs/core";
+import React, { useState, useEffect } from 'react';
+import styles from './ResultScreen.module.css';
+import { useLocation, Link } from 'react-router-dom';
+import { Button } from '@blueprintjs/core';
+import axios from 'axios';
 
 function ResultScreen() {
+  const [result, setResult] = useState('');
   const data = useLocation();
   const protein = data.protein;
-  const genome = data.genome;
-
+  const genome = data.genome.text;
   const isValid = protein !== undefined && genome !== undefined;
+  console.log(result);
+
+  useEffect(() => {
+    test();
+  }, []);
+  const test = async () => {
+    await axios
+      .post(`/matrix/${protein.protein.matrix_id}/PPM/5`, {
+        dna: genome
+      })
+      .then(function(response) {
+        let topArray = response.data;
+        //let returnArray;
+        if (topArray === 'Something went wrong. Sorry about that.') {
+          console.log(topArray);
+        } else {
+          setResult({
+            name: 'Results',
+            params: {
+              data: topArray
+            }
+          });
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
 
   const homeButton = (
     <div className={styles.homeButton}>
       <Link
         to={{
-          pathname: "/"
-        }}
-      >
-        <Button intent="primary" icon="home" minimal="true" large="true">
+          pathname: '/'
+        }}>
+        <Button intent='primary' icon='home' minimal='true' large='true'>
           Home
         </Button>
       </Link>
@@ -30,13 +58,13 @@ function ResultScreen() {
       <h1>Result</h1>
       <p>
         {isValid &&
-          "The result of " +
+          'The result of ' +
             protein.protein.name +
-            " and genome " +
+            ' and genome ' +
             genome +
-            ":"}
+            ':'}
         {!isValid &&
-          "Not valid, protein or genome is undefined. Start over by pressing the Home-button."}
+          'Not valid, protein or genome is undefined. Start over by pressing the Home-button.'}
       </p>
     </div>
   );
