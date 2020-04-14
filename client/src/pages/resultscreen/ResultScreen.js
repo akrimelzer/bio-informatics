9fa052035834977864537bb4ec1dc924aadf988b
Andreas Melzer <akrimelzer@gmail.com>
2020-04-14 12:24:46 +0200

    Add PWM to results

--- client/src/pages/resultscreen/ResultScreen.js
+++ client/src/pages/resultscreen/ResultScreen.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import styles from "./ResultScreen.module.css";
-import { useLocation, Link } from "react-router-dom";
-import { Button, Card, Elevation, Spinner } from "@blueprintjs/core";
-import { Chart } from "react-charts";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from './ResultScreen.module.css';
+import { useLocation, Link } from 'react-router-dom';
+import { Button, Card, Elevation, Spinner } from '@blueprintjs/core';
+import { Chart } from 'react-charts';
 
 function ResultScreen() {
   const [isLoading, setIsLoading] = useState(true);
@@ -32,23 +32,28 @@ function ResultScreen() {
   }, []);
   const chartData = [
     {
-      label: "Probability",
-      data: results.chart_arr,
+      label: 'Probability',
+      data: results.chart_arr_PPM,
+    },
+  ];
+  const chartData_PWM = [
+    {
+      label: 'Probability',
+      data: results.chart_arr_PWM,
     },
   ];
   const axes = [
-    { primary: true, type: "linear", position: "bottom" },
-    { type: "linear", position: "left" },
+    { primary: true, type: 'linear', position: 'bottom' },
+    { type: 'linear', position: 'left' },
   ];
 
   const homeButton = (
     <div className={styles.homeButton}>
       <Link
         to={{
-          pathname: "/",
-        }}
-      >
-        <Button intent="primary" icon="home" minimal="true" large="true">
+          pathname: '/',
+        }}>
+        <Button intent='primary' icon='home' minimal='true' large='true'>
           Home
         </Button>
       </Link>
@@ -61,18 +66,18 @@ function ResultScreen() {
       <h1>Result</h1>
       <p>
         {isValid &&
-          "The result of finding most likely transcription factor binding sited between " +
+          'The result of finding most likely transcription factor binding sited between ' +
             protein.protein.name +
-            " and genome " +
+            ' and genome ' +
             genome +
-            ":"}
+            ':'}
         {!isValid &&
-          "Not valid, protein or genome is undefined. Start over by pressing the Home-button."}
+          'Not valid, protein or genome is undefined. Start over by pressing the Home-button.'}
       </p>
       <Card className={styles.resultContainer} elevation={Elevation.THREE}>
         <div className={styles.topResults}>
-          <h1>Most likely bindings</h1>
-          {results.top_x.map((element) => (
+          <h1>Most likely bindings(PPM)</h1>
+          {results.top_x_PPM.map((element) => (
             <div key={element.id} className={styles.element}>
               <h4>{element.name}</h4>
               <p>Position: {element.position}</p>
@@ -90,10 +95,31 @@ function ResultScreen() {
           <Chart data={chartData} axes={axes} tooltip />
         </div>
       </Card>
+      <Card className={styles.resultContainer} elevation={Elevation.THREE}>
+        <div className={styles.topResults}>
+          <h1>Most likely bindings(PWM)</h1>
+          {results.top_x_PWM.map((element) => (
+            <div key={element.id} className={styles.element}>
+              <h4>{element.name}</h4>
+              <p>Position: {element.position}</p>
+              <p>Score: {element.value}</p>
+            </div>
+          ))}
+        </div>
+        <div className={styles.chart}>
+          <h1>Probabilities</h1>
+          <div className={styles.infotext}>
+            <p>X-axis: Positions in the genome</p>
+            <p>Y-axis: Probability for binding in the current position</p>
+          </div>
+
+          <Chart data={chartData_PWM} axes={axes} tooltip />
+        </div>
+      </Card>
     </div>
   ) : (
     <div className={styles.spinner}>
-      <Spinner size="70" intent="primary" />
+      <Spinner size='70' intent='primary' />
       <h1>May take some time...</h1>
       <p>Finding the most likely transcription factor binding sites.</p>
     </div>
